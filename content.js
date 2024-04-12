06f218434cf9a5030deaa54f15b97b8a8729aaad
nirmal-92 <nn3134395@gmail.com>
2024-04-12 13:52:10 +0530

    commit

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ document.addEventListener("mouseover", function(event) {
   if (event.target.tagName === "A") {
     checkLinkSafety(event.target.href, function(isSafe) {
       if (isSafe) {
-        event.target.title = "This link is safe";
+        event.target.title = "This";
       } else {
         event.target.title = "This link is not safe";
       }
