a62cf3187519f6db7b742e68a5714421bc21157c
nirmal-92 <nn3134395@gmail.com>
2024-04-15 12:27:15 +0530

    commit

--- content.js
+++ content.js
@@ -1,13 +1,15 @@
-  document.getElementById("tooltip").addEventListener("mouseover", function() {
-  const anchorElement = document.querySelector("#tooltiptext a");
-  if (anchorElement) {
-    checkLinkSafety(anchorElement.href, function(isSafe) {
-      anchorElement.title = isSafe ? "✅ Safe Link" : "❌ Unsafe Link";
-    });
-  }
-});
+document.getElementById("tooltip").addEventListener("mouseover", handleMouseOver);
+
+function handleMouseOver() {
+    const anchorElement = document.querySelector("#tooltiptext a");
+    if (anchorElement) {
+        checkLinkSafety(anchorElement.href, function(isSafe) {
+            anchorElement.title = isSafe ? "✅ Safe Link" : "❌ Unsafe Link";
+        });
+    }
+}
 
 function checkLinkSafety(url, callback) {
-  const isSafe = url.startsWith("https://");
-  callback(isSafe);
+    const isSafe = url.startsWith("https://");
+    callback(isSafe);
 }
